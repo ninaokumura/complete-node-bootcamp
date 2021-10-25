6ce2c07dc066c97635c443291c3e48c1fa7b9394
Nina Okumura <elaineokumura@gmail.com>
2021-10-25 13:20:43 +1300

    add routes

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,74 +1,110 @@
-// File system module return an object with functions we can use
-const fs = require('fs')
-const http = require('http')
-const url = require('url')
+// built in modules
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
-/////////////////////////////////////////////////////////////////////////////////////////
-// Files
+//////////////////////////////////////////////////////////////////////
+//Files
 
 /*
-// synchronus Blocking
-// This will read the data from the file and return it
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
-console.log(textIn)
-
-// //Write in files
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
-fs.writeFileSync('./txt/outup.txt', textOut)
-console.log('File written!', textOut)
+// Reading files
+//Blocking synchronous way
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+// writing files
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File written!');
 */
-//////////////////////////////////////////////////////////////////////////////////////////
 /*
-// Asynchronus non-blocking
+// Non blocking Asynchronous way
+// Read file
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  if (err) return console.log('ERROR!💥')
+  if (err) return console.log('ERROR!💥');
   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2)
+    console.log(data2);
     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-      console.log(data3)
+      console.log(data3);
 
+      // Write file
       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-        console.log('Your file has been written 😀')
-      })
-    })
-  })
-})
-
-console.log('Will read file!')
+        console.log('Your file has been written!');
+      });
+    });
+  });
+});
+console.log('Will read file!');
 */
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Creating a web server
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
 
-// Creating a simple web server
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+};
+// The top level code wuill only be executed once we start the program. So we can use the sync version...
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
-const dataObj = JSON.parse(data)
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  console.log(req.url)
+  const { query, pathname } = url.parse(req.url, true);
 
-  const pathName = req.url
-  if (pathName === '/' || pathName === '/overview') {
-    res.end('This is the overview!')
-  } else if (pathName === '/product') {
-    res.end('This is the product!')
-  } else if (pathName === '/api') {
-    res.writeHead(200, {
-      'Content-type': 'application/json',
-    })
-    res.end(data)
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+
+    // Product page
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    // Retrieve an element based on query string
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    // Building a simple api
+  } else if (pathname === '/api') {
+    fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+      res.writeHead(200, { 'Content-type': 'application/json' });
+      res.end(data);
+    });
+    // Not found
   } else {
     res.writeHead(404, {
-      // http header
       'Content-type': 'text/html',
       'my-own-header': 'hello-world',
-    })
-    res.end('<h1>Page not found!</h1>')
+    });
+    res.end('<h1>Page not found!</h1>');
   }
-})
+});
 
 server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening to requests on port 8000')
-})
+  console.log('Listening to requests on port 8000');
+});
 
-// Routing simply means implement different actions for different urls
-
-// Web api => service where we can request data
+// Routing simply means implement different actions for different URLs
