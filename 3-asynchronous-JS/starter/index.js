04925b9808a96846e48f576f01bd71a4ee2779c3
Nina Okumura <elaineokumura@gmail.com>
2021-10-27 16:24:53 +1300

    implement multiple promises

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -20,7 +20,59 @@ const writeFilePro = (file, data) => {
     });
   });
 };
+// Consuming promises with async/ await
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
 
+    const res1Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const res2Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const res3Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    const imgs = all.map((el) => el.body.message);
+    console.log(imgs);
+
+    await writeFilePro("dog-img.txt", imgs.join("\n"));
+    console.log("Random dog image saved to the file!");
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+  return "2: Ready!";
+};
+
+(async () => {
+  try {
+    console.log("1: Will get dog pics!");
+    const x = await getDogPic();
+    console.log(x);
+    console.log("3: Done getting dog pics!");
+  } catch (err) {
+    console.log("Error");
+  }
+})();
+/*
+console.log("1: Will get dog pics!");
+getDogPic()
+  .then((x) => {
+    console.log(x);
+    console.log("3: Done getting dog pics!");
+  })
+  .catch((err) => {
+    console.log("ERROR");
+  });
+*/
+/*
 readFilePro(`${__dirname}/dog.txt`)
   .then((data) => {
     console.log(`Breed: ${data}`);
@@ -37,5 +89,7 @@ readFilePro(`${__dirname}/dog.txt`)
   .catch((err) => {
     console.log(err.message);
   });
-
+*/
 // How to solve callback hell using promises
+
+//Returning values from async/await
