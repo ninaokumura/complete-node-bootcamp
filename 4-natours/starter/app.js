6f73cc20bb60308ff41e8d33c21144bf74851f23
Nina Okumura <elaineokumura@gmail.com>
2021-10-28 17:43:25 +1300

    add config.env file

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -11,9 +11,14 @@ const app = express();
 
 // Middleware
 // Using third party middleware => this case morgan
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware!');
