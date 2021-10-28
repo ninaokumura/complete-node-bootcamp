53505c5165faf83335827c748ea0a76d4834e10d
Nina Okumura <elaineokumura@gmail.com>
2021-10-28 13:53:45 +1300

    handling delete requests

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -78,6 +78,38 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+// Handling patch requests to update data
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+});
+
+// Handling delete requests
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 const port = 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
