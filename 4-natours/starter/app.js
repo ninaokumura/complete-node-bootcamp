f65e0d0b9a863d75669ca67348f4d0a8f84c3249
Nina Okumura <elaineokumura@gmail.com>
2021-10-28 13:36:22 +1300

    Responding to url params

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,7 +1,11 @@
 // It is a convention to have all the Express config in app.js file
 const fs = require('fs');
 const express = require('express');
+const { fail } = require('assert');
 const app = express();
+
+// Middleware
+app.use(express.json());
 
 // Define routes => determine how an application responds to a certain client request, so to a certain url
 // app.get('/', (req, res) => {
@@ -28,6 +32,50 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+// Responding to url parameters => Getting only one tour
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1; // When you multiply a str number to any number, js automatically converts the number
+  const tour = tours.find((el) => el.id === id);
+
+  // if (id > tours.length)
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+// Handling post requests
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 8000;
